85fc0f33818537a1aae413cb26605487e3d3b455
Satosh <smartdev.house108@gmail.com>
2023-11-25 08:30:06 -0800

    Day3 morning Complete nested routing.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,11 +3,29 @@ import {
     RouterProvider,
 } from 'react-router-dom';
 import { WelcomePage } from './pages/Welcome';
+import App from './App';
+import UsersPage from './pages/Users';
+import AboutPage from './pages/About';
 
 const router = createBrowserRouter([
+
     {
-        path: 'welcome',
-        element: <WelcomePage />,
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: 'welcome',
+                element: <WelcomePage />,
+            },
+            {
+                path: 'users',
+                element: <UsersPage />
+            },
+            {
+                path: 'about',
+                element: <AboutPage />
+            },
+        ]
     },
 ]);
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import { NavLink } from 'react-router-dom';
+
+
+export const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-sm bg-light">
+            <div className="container-fluid">
+                <ul className="navbar-nav">
+                    <a className="navbar-brand" href="#">LOGO</a>
+                    <li className="nav-item">
+                        <NavLink
+                            className={({ isActive }) => { return `nav-link ${isActive && 'active'}` }}
+                            to="/welcome"
+                            children='Welcome'
+                        />
+                    </li>
+                    <li className="nav-item"
+                    >
+                        <NavLink
+                            className={({ isActive }) => { return `nav-link ${isActive && 'active'}` }}
+                            to="/users"
+                            children='Users'
+                        />
+                    </li>
+                    <li className="nav-item">
+                        <NavLink
+                            className={({ isActive }) => { return `nav-link ${isActive && 'active'}` }}
+                            to="/about"
+                            children='About'
+                        />
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
