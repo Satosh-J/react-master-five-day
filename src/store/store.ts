f77c31ce4eff974940c86de10ea234c5f11c204e
Satosh <smartdev.house108@gmail.com>
2023-11-30 16:02:08 -0600

    day5-pm-saga-configuration-complete

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,19 @@
-import { createStore, Store } from 'redux';
-import rootReducer from './rootReducer';
 import { composeWithDevTools } from '@redux-devtools/extension';
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './rootSaga';
+import { createStore, applyMiddleware, Store } from 'redux';
+import rootReducer from './rootReducer';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store: Store = createStore(
   rootReducer,
-  composeWithDevTools()
+  composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+  )
+
 );
+
+sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
